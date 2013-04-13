8a6a71da62a672226447514f52c2376513ef0b8a
Adam Gotterer <agotterer@gmail.com>
2013-04-13 12:18:48 -0400

    Added a path option

--- _plugins/TocFilter.rb
+++ _plugins/TocFilter.rb
@@ -1,11 +1,11 @@
 module TocFilter
-  def toc(input)
-    output = "<ul class=\"toc\">"
+  def toc(input, path='')
+    output = "<ul class=\"nav nav-list toc\">"
     input.scan(/<(h2)(?:>|\s+(.*?)>)([^<]*)<\/\1\s*>/mi).each do |entry|
       id = (entry[1][/^id=(['"])(.*)\1$/, 2] rescue nil)
       title = entry[2].gsub(/<(\w*).*?>(.*?)<\/\1\s*>/m, '\2').strip
       if id
-        output << %{<li><a href="##{id}">#{title}</a></li>}
+        output << %{<li><a href="#{path}##{id}">#{title}</a></li>}
       else
         output << %{<li>#{title}</li>}
       end
